c2074aa7e6500e2bf8f1b889c26bf528d0e1d224
Takanashi <nao69alter@gmail.com>
2025-10-20 18:38:44 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Top from './pages/Top'
+
+function App() {
+
+  return (
+    <>
+      <Top />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Top.jsx
@@ -0,0 +1,21 @@
+import '../styles/top.css'
+
+export default function Top() {
+    return (
+        <><h1 className="loading-title">React & Vite & Vercel</h1><div className="loading-container">
+
+
+            <h2 className="loading-text">Loading...</h2>
+            <p className="loading-description">
+                アプリケーションを読み込んでいます...<br />
+                しばらくお待ちください
+            </p>
+
+            <div className="dots-container">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+            </div>
+        </div></>
+    )
+};
