179dfb4cb3ca550f7e5d47ac4ec30d874aa666f4
Takanashi <nao69alter@gmail.com>
2025-12-09 13:53:56 +0900

    画面遷移する機能を付与

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Top from "./pages/Top";
+import Home from "./pages/Home"; // 新規に必要
 
 function App() {
   return (
-    <>
-      <Top />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -1,13 +1,22 @@
 import "../styles/top.css";
 import cat from "../assets/cat.svg";
 import tiger from "../assets/tiger.svg";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Top() {
   const animal = import.meta.env.DEV ? cat : tiger;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/home");
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
   return (
     <>
-      <h1 className="loading-title">React & Vite & Vercel</h1>
       <div className="loading-container">
         {/* ロゴ */}
         <div className="logo-container">
